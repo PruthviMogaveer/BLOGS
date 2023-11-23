6e9d6eb71bbb1a5cf300e011e0a97e3bf879afb7
PruthviMogaveer <pruthvimogaveer18@gmail.com>
2023-11-23 14:54:00 +0530

    completed

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 import Header from "./header/Header";
-import Footer from "./footer/footer";
+import Footer from "./footer/Footer";
 import Container from "./container/Container";
 import LogoutBtn from "./header/LogoutBtn";
 import Button from "./Button";
