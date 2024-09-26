be0a7e72550065b0d20c7eb7537a3c6cfeb043fe
Pruthvi <pruthvimogaveer18@gmail.com>
2024-09-26 12:27:04 +0530

    added api key for tinymce

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -17,6 +17,7 @@ const RTE = ({ name, label, control, defaultValue = "" }) => {
           return (
             <Editor
               initialValue={defaultValue}
+              apiKey='fp5loth3q7h69xjp9u5ehccwb0yuyyq1o3b6ft0ejswxylc5'
               init={{
                 mobile: {
                   height: 350,
